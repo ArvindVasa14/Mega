e582f4ad6bb1baf5532cbb06ec74d7fe163dd56f
arvindvasa1122@gmail.com <arvindaiml14@gmail.com>
2024-04-15 20:55:04 +0530

    first commit

---
+++ src/main/java/com/pack/Mega/MegaApplication.java
@@ -0,0 +1,13 @@
+package com.pack.Mega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegaApplication.class, args);
+	}
+
+}
