1c0ea18d4d37c3204164330a3d86b6853013d122
arvindvasa1122@gmail.com <arvindaiml14@gmail.com>
2024-04-16 14:19:38 +0530

    Entities modified 2

--- src/main/java/com/pack/Mega/model/UserDetail.java
+++
@@ -1,5 +0,0 @@
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
