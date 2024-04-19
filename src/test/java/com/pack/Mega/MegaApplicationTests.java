4e5bf2a490ec75d83078f101a5d7da4a16178d5c
arvindvasa1122@gmail.com <arvindaiml14@gmail.com>
2024-04-19 13:56:25 +0530

    dummy

--- src/test/java/com/pack/Mega/MegaApplicationTests.java
+++ src/test/java/com/pack/Mega/MegaApplicationTests.java
@@ -1,13 +1,18 @@
 package com.pack.Mega;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
 class MegaApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }
