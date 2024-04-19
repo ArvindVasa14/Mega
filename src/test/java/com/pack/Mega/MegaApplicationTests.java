6b09c58d23a301da83eaddb40be75e0f0d6562ed
arvindvasa1122@gmail.com <arvindaiml14@gmail.com>
2024-04-19 15:56:00 +0530

    context loads

--- src/test/java/com/pack/Mega/MegaApplicationTests.java
+++ src/test/java/com/pack/Mega/MegaApplicationTests.java
@@ -6,12 +6,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
-@RunWith(SpringRunner.class)
 class MegaApplicationTests {
 
-	@Test
-	void contextLoads() {
-
-	}
+//	@Test
+//	void contextLoads() {
+//
+//	}
 
 }
