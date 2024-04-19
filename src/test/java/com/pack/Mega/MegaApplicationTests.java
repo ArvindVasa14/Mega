73526388570dd42f7977d96ba7ab042faf35b659
arvindvasa1122@gmail.com <arvindaiml14@gmail.com>
2024-04-19 14:04:32 +0530

    yml

--- src/test/java/com/pack/Mega/MegaApplicationTests.java
+++ src/test/java/com/pack/Mega/MegaApplicationTests.java
@@ -3,7 +3,6 @@ package com.pack.Mega;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
